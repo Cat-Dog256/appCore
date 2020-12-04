4f708eb94b8e6a7b0ca53c530c1563dd846246ba
Lice <lice_home@163.com>
2020-12-04 16:19:02 +0800

    难受

--- LKAppCore/LKAppCore/ViewController.m
+++ LKAppCore/LKAppCore/ViewController.m
@@ -15,6 +15,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    LKRequest *request = [LKRequest createGetRequestWithrequestUrl:@"11"];
+    request.ignoreToken = YES;
+    [request startWithCompletionBlockWithSuccess:^(LKRequest * _Nonnull request) {
+            
+        } failure:^(LKRequest * _Nonnull request) {
+            
+        }];
     // Do any additional setup after loading the view.
 }
 
